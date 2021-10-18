143dddc6d9f1856fa05821355b1c247c28621f01
rromulo <rromulo.silva21@gmail.com>
2021-10-18 16:17:49 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        280:{
+            items:2
+        },
+        580:{
+            items:3
+        },
+        768:{
+            items:5
+        }
+    },
+})
